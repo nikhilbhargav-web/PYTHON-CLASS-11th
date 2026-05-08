254aaa2438683c5187662d841fcc81bf2042e8be
nikhilbhargav-web <nikhilbhargav-web@users.noreply.github.com>
2026-05-08 20:58:24 +0530

    Create day-04-table-generator.py

---
+++ day-04-table-generator.py
@@ -0,0 +1,19 @@
+# DAY 4: TABLE GENERATOR BY NIKHIL BHARGAV
+# GOAL : GERMANY IT AUSBILDUNG 2028
+
+print("=========================================")
+print("       IGL TABLE GENERATOR - DAY 4"     )
+print("=========================================")
+
+# USER SE NUMBER INPUT LO
+num = int(input("table kis number ka chaiya? daalo :"))
+
+print(f"\n----- {num} ka table -----")
+# For loop: 1 se 10tak chalao
+for i in range(1, 11):
+    result = num * i
+    print(f"{num} x {i} = {result}")
+
+print("---------------------------------")
+print("IGL DAY 4 COMPLETE")
+print("Next : Github pe upload")
